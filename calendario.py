fa55927428c49fc215409fa5e4e4af7dfa435b9c
ValenBcn <131005709+ValenBcn@users.noreply.github.com>
2025-03-12 17:07:31 +0100

    Update calendario.py

--- calendario.py
+++ calendario.py
@@ -66,13 +66,13 @@ st.markdown(
         
         /* Ajuste del ancho de los selectores */
         .filters-container select {
-            width: 150px !important;  /* Cambia el valor para ajustar */
+            width: 120px !important;  /* Cambia el valor para ajustar */
             height: 36px;
             font-size: 14px;
         }
 
         .stSelectbox {
-            width: 150px !important;  /* Cambia el valor para ajustar */
+            width: 120px !important;  /* Cambia el valor para ajustar */
         }
 
         /* Mejor distribución entre filtros y calendario */
